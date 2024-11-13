ee264b16f1aac98c862a8877ff36b3e0aa80b5f7
stxtory <stxtory@gmail.com>
2024-11-13 16:50:14 +0900

    01장 디자인 패턴 소개와 전략 패턴 | 디자인 패턴의 세계로 떠나기

---
+++ ModelDuck.java
@@ -0,0 +1,12 @@
+public class ModelDuck extends Duck{
+
+    public ModelDuck() {
+        this.flyBehavior = new FlyNoWay();
+        this.quackBehavior = new MuteQuack();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("저는 모형 오리입니다.");
+    }
+}
